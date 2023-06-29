9b9c04938dfe6cc011364ea3f9c66e8bdd901696
moonjunsain <moonjunsain@gmail.com>
2023-06-29 12:15:50 -0700

    Done

---
+++ script.js
@@ -0,0 +1,47 @@
+// const playButton = document.querySelector("#Play");
+// playButton.addEventListener("click", playGame());
+
+function compSelect() {
+    var compChoice = ['R', 'P', 'S'];
+    var randomChoice = Math.floor(Math.random() * compChoice.length);
+
+    return compChoice[randomChoice];
+}
+
+var stats = {
+wins: 0,
+loses: 0,
+ties: 0,
+totalGames: 0,
+};
+
+function decideWinner(computer, user){
+    
+
+    if(user === computer){
+        alert("It's a tie!");
+        stats.ties++;
+    } else if((user === 'r' && computer === 'S') || (user === 'p' && computer === 'R') || (user === 's' && computer === 'P')){
+        alert("You Win!");
+        stats.wins++;
+    } else {
+        alert("You Lose");
+        stats.loses++;
+    }
+    stats.totalGames++;
+
+    alert(`Wins: ${stats.wins} \nLoses: ${stats.loses} \nTies: ${stats.ties}\nTotal Game Played: ${stats.totalGames}`);
+}
+
+
+function playGame(){
+    var userChoice = prompt("R, P, or S");
+  decideWinner(compSelect(), userChoice);  
+}
+
+
+do {
+    playGame();
+    var replay = confirm("Do you wish to replay?");
+    
+} while (replay);
